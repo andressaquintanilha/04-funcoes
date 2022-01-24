563b3af0d63b9382648915fef062f4e099fc854e
andressaquintanilha.email <quintanilhalg3@gmail.com>
2022-01-24 08:57:28 -0300

    Pequenas alterações no Js

--- script.js
+++ script.js
@@ -8,22 +8,22 @@ function mensagem1(name) {
     console.log (`Olá ${name}, como vai? Seja bem vindo ao vai na web!`)
 }
 
-console.log(mensagem1("Andressa"))
+mensagem1("Andressa")
 
 //Crie uma função que receba um nome, um número, e um estilo musical (parâmetros) e exiba no console.
 function mensagem2(name, number, musical) {
     console.log (`O(a) aluno(a) ${name} possui ${number} anos e prefere o estilo musical ${musical}.`)
 }
-console.log(mensagem2("Matheus", 24, "pop-rock"))
-console.log(mensagem2("Bianca", 32, "sertanejo universitário"))
-console.log(mensagem2("Juliana", 27, "funk"))
+mensagem2("Matheus", 24, "pop-rock")
+mensagem2("Bianca", 32, "sertanejo universitário")
+mensagem2("Juliana", 27, "funk")
 
 //Crie uma função que receba um filme, uma música (parâmetros) e exiba no console.
 function mensagem3(film, musicalFilm) {
     console.log(`O filme "${film}" tem como música tema a canção ${musicalFilm}.`)
 }
-console.log(mensagem3("Titanic", " My Heart Will Go On - Céline Dion"))
-console.log(mensagem3("Missão Impossível", "I Will Always Love You - Whitney Houston"))
+mensagem3("Titanic", " My Heart Will Go On - Céline Dion")
+mensagem3("Missão Impossível", "I Will Always Love You - Whitney Houston")
 
 //  
 function bonus(number) {
